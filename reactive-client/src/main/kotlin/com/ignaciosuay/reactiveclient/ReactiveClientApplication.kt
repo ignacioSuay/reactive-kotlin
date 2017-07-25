51814cee91517c2a7974dd6370d180345d966aa9
Ignacio Suay <ignacio.suay@gmail.com>
2017-07-25 18:34:06 +0100

    generate the client

--- reactive-client/src/main/kotlin/com/ignaciosuay/reactiveclient/ReactiveClientApplication.kt
+++ reactive-client/src/main/kotlin/com/ignaciosuay/reactiveclient/ReactiveClientApplication.kt
@@ -2,10 +2,30 @@ package com.ignaciosuay.reactiveclient
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.web.reactive.function.client.WebClient
+
 
 @SpringBootApplication
 class ReactiveClientApplication
 
+
 fun main(args: Array<String>) {
     SpringApplication.run(ReactiveClientApplication::class.java, *args)
+}
+
+@Bean
+fun client(): WebClient = WebClient.create("http:://localhost:8080")
+
+@Bean
+fun testClient() {
+    println("hey starting")
+
+    client()
+            .get()
+            .uri("hola")
+            .exchange()
+            .flatMap { cr -> 1 }
+            .subscribe(println())
+
 }
