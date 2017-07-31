d0d65893b2bef55aeb5afdbf134386ca3ea5c5c6
Ignacio Suay <ignacio.suay@gmail.com>
2017-07-31 17:29:51 +0100

    clean client

--- reactive-client/src/main/kotlin/com/ignaciosuay/reactiveclient/ReactiveClientApplication.kt
+++ reactive-client/src/main/kotlin/com/ignaciosuay/reactiveclient/ReactiveClientApplication.kt
@@ -5,50 +5,35 @@ import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.Bean
-import org.springframework.http.MediaType.TEXT_EVENT_STREAM
-import org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
-import org.springframework.web.reactive.function.client.bodyToMono
 import java.time.LocalDateTime
 
 @JsonDeserialize
 data class Event(val id: Int, val time: LocalDateTime)
 
 @SpringBootApplication
-class ReactiveClientApplication{
-
-
-//    @Bean
-//    fun client(): WebClient {
-//        println("hola")
-//        return  WebClient.create("http:://localhost:8082")
-//    }
+class ReactiveClientApplication {
 
     @Bean
     fun init(): CommandLineRunner = CommandLineRunner {
         val client = WebClient.create("http://localhost:8082")
-//        client
-//                .get()
-//                .uri("/events/22")
-//                .retrieve()
-//                .bodyToMono<Event>()
-//                .subscribe({ e -> println("my event" + e) })
-//    }
+
         client
                 .get()
                 .uri("/events")
-                .accept(TEXT_EVENT_STREAM)
+//                .accept(TEXT_EVENT_STREAM)
                 .retrieve()
                 .bodyToFlux<Event>()
                 .subscribe({ e -> println("my event" + e) })
     }
-}
 
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(ReactiveClientApplication::class.java, *args)
 }
+
 
 
 
